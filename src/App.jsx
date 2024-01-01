9dbe08900bb1682c2de8b4ffd8d821fb8947e1df
shoaib khan <shoaib.khan.880066@gmail.com>
2024-01-01 21:35:58 +0530

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import angle from "./assets/angle_border.svg";
 import tops from "./assets/tops.svg";
+import line from "./assets/line.svg";
 import "./App.css";
 const App = () => {
   return (
@@ -13,20 +14,27 @@ const App = () => {
         <img src={angle} alt="angle" className="flipped" />
       </div>
 
-      <div className="middle_deco">
-        <img className="tops" src={tops} alt="tops_deco" />
+      <div className="middle_deco_container">
+        <img className="top" src={tops} alt="tops_deco" />
       </div>
 
-      <div className="middle_deco">
-        <img className="title_bottom_deco" src={tops} alt="tops_deco" />
-      </div>
-
-      <div className="res_title">
-        <p className="restaurant_name">DAAWAT </p>
+      <div className="res_title_container">
+        <span className="restaurant_name">DAAWAT </span>
       </div>
 
       <div className="res_desc_container">
-        <p className="restaurant_desc">FAMILY RESTAURANT</p>
+        <span className="restaurant_desc">FAMILY RESTAURANT</span >
+      </div>
+
+      <div className="bottom_deco_container">
+        <img className="bottom" src={tops} alt="tops_deco" />
+      </div>
+
+      <div className="left_line_container">
+        <img alt="line" src={line} className="line_left" />
+      </div>
+      <div className="right_line_container">
+        <img alt="line" src={line} className="line_right" />
       </div>
     </div>
   );
